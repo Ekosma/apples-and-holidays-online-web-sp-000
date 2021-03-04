84a7685c406f0c2b17d376b0bbeb39812a661a89
Evelyn Kosma <ezranaja@gmail.com>
2021-03-04 22:27:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -99,21 +99,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  {
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
+ def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten.compact
 end
 
 
